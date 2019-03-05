e3a1631e6480316e017379e9f191c3325db3c03a
Patrick Stegmann <patrick.stegmann@zuehlke.com>
2019-03-05 16:07:36 +0100

    A little test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { QuillModules } from 'ngx-quill';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'quilltesting';
+  quillConfig: QuillModules = {
+    toolbar: {
+      container: [
+        ['bold', {'list': 'bullet'}],
+      ]
+    }
+  };
+
+  form: FormControl = this.fb.control('');
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  onContentChanged(event) {
+    this.form.setValue(event.html);
+  }
+
+  onPaste(event) {
+    console.info(event);
+  }
+
+  save() {
+    console.info('Completed content of the editor is', this.form.value);
+  }
 }
